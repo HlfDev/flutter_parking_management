ecd6072375dd9da499e7dfa3c8f4c00dcd88b629
Pedro Henrique <pedrohlf.contato@gmail.com>
2023-01-20 00:22:11 -0300

    chore(modules): add app module,core module and  app widget

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Parking Management',
+      debugShowCheckedModeBanner: false,
+      supportedLocales: const [Locale('pt', 'BR')],
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
+  }
+}
