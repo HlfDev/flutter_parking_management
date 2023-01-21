56ada122b73c49aa3e1e4829257fd698d8b00e33
Pedro Henrique <pedrohlf.contato@gmail.com>
2023-01-21 00:25:52 -0300

    chore(controller/module): add app navigator into splash bloc and add bloc into splash module

--- lib/app/modules/splash/presentation/controller/splash_bloc.dart
+++ lib/app/modules/splash/presentation/controller/splash_bloc.dart
@@ -1,11 +1,17 @@
 import 'package:bloc/bloc.dart';
 
+import '../../../../app.dart';
+
 part 'splash_event.dart';
 part 'splash_state.dart';
 
 class SplashBloc extends Bloc<SplashEvent, SplashState> {
-  SplashBloc() : super(SplashInitialState()) {
+  final AppNavigator _appNavigator;
+
+  SplashBloc(this._appNavigator) : super(SplashInitialState()) {
     on<SplashStartLoadEvent>((event, emit) => emit(SplashStartLoadState()));
     on<SplashStopLoadEvent>((event, emit) => emit(SplashStopLoadState(isLoaded: true)));
   }
+
+  void navigateToMainModule() => _appNavigator.navigateToMainModule();
 }
--- lib/app/modules/splash/splash_module.dart
+++ lib/app/modules/splash/splash_module.dart
@@ -1,12 +1,21 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../core/core.dart';
+
+import 'presentation/controller/controller.dart';
 import 'presentation/pages/pages.dart';
 
 class SplashModule extends Module {
   static const moduleName = '/';
 
   @override
-  final List<Bind> binds = [];
+  List<Module> get imports => [CoreModule()];
+
+  @override
+  final List<Bind> binds = [
+    // Controller
+    Bind.lazySingleton((i) => SplashBloc(i())),
+  ];
 
   @override
   final List<ModularRoute> routes = [
