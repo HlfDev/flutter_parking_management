9e15236b8cbf844a155c78e40e12fe4ef9b16abb
Pedro Henrique <pedrohlf.contato@gmail.com>
2023-01-20 21:11:53 -0300

    feat(page): add initial splash page and add route to splash module

--- lib/app/modules/splash/splash_module.dart
+++ lib/app/modules/splash/splash_module.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'presentation/pages/pages.dart';
+
 class SplashModule extends Module {
   static const moduleName = '/';
 
@@ -7,5 +9,10 @@ class SplashModule extends Module {
   final List<Bind> binds = [];
 
   @override
-  final List<ModularRoute> routes = [];
+  final List<ModularRoute> routes = [
+    ChildRoute(
+      SplashPage.routeName,
+      child: (_, args) => const SplashPage(),
+    ),
+  ];
 }
